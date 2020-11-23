f6ce9f36911c229e396ac16d0d28cba81c0e9fd5
e-zk <58356365+e-zk@users.noreply.github.com>
2020-11-23 18:26:51 +1000

    rewrote xrdb function

--- templates/functions/main.go
+++ templates/functions/main.go
@@ -7,13 +7,15 @@ import (
 	"text/template"
 )
 
-func xrdbGrep(reg string) string {
+func xrdbValue(substr string) string {
 	xrdbCmd := exec.Command("xrdb", "-query")
 
 	output, err := xrdbCmd.Output()
 	if err != nil {
 		panic(err)
 	}
+
+	reg := substr + ":\t(.*)"
 
 	r := regexp.MustCompile(reg)
 	match := r.FindStringSubmatch(fmt.Sprintf("%s", output))
@@ -23,6 +25,6 @@ func xrdbGrep(reg string) string {
 
 func GetFuncs() template.FuncMap {
 	return template.FuncMap{
-		"XrdbGrep": xrdbGrep,
+		"XrdbValue": xrdbValue,
 	}
 }
